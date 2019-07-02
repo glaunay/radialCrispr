c537886ca926b083f3a361ede8827dc0a7c2d356
sophielem <lematre.sophiegmail.com>
2019-07-02 15:53:08 +0200

    first commit

---
+++ src/components/my-component/my-component.tsx
@@ -0,0 +1,31 @@
+import { Component, Prop, State, Element} from '@stencil/core';
+// import fs from 'fs';
+
+
+@Component({
+  tag: 'radial-crispr',
+  styleUrl: 'my-component.css',
+  shadow: true
+})
+
+export class MyComponent {
+// *************************** PROPERTY & CONSTRUCTOR ***************************
+
+// *************************** LISTEN & EMIT ***************************
+
+
+
+// *************************** CLICK ***************************
+
+
+// *************************** DISPLAY ***************************
+
+  render() {
+    console.log("rendr called")
+
+
+    return ([
+
+]);
+  }
+}
